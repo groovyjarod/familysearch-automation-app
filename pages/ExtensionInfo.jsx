082787aa5136449aa77d0885b6c1336f790876be
groovyjarod <jarodd99@gmail.com>
2026-04-08 09:15:23 -0600

    added functionality to additionally deploy mac app

--- pages/ExtensionInfo.jsx
+++ pages/ExtensionInfo.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Button, Menu, VStack, HStack } from '@chakra-ui/react'
+import CenteredVstackCss from '../reusables/CenteredVstackCss'
 import BodyVstackCss from '../reusables/BodyVstackCss'
 import BodyHstackCss from '../reusables/BodyHstackCss'
 import MenuHeader from '../reusables/MenuHeader'
@@ -10,15 +11,17 @@ const ExtensionInfo = () => {
         window.electronAPI.getVersion().then(setVersion)
     }, [])
     return (
-        <VStack {...BodyVstackCss}>
+        <VStack {...CenteredVstackCss}>
             <MenuHeader title="Download Extension" subTitle={`Version ${version}`} />
             <HStack {...BodyHstackCss}>
                 <h1>Chrome Extension Tool</h1>
                 <button className='btn btn-small btn-files' onClick={() => window.electronAPI.openChromeUrl('https://github.com/groovyjarod/Audit-Extension')}>Go to site →</button>
             </HStack>
-            <h2>Visualize your audits' results on the webpage you audited.</h2>
-            <h3>This extension takes the data generated from the .json files created from your audits, and displays the locations of Accessibility errors throughout the page.</h3>
-            <h3>Refer to the README.md towards the bottom of the webpage for full instructions on installation and usage.</h3>
+            <VStack {...BodyVstackCss}>
+                <h3>Visualize your audits' results on the webpage you audited.</h3>
+                <p>This extension takes the data generated from the .json files created from your audits, and displays the locations of Accessibility errors throughout the page.</p>
+                <p>Refer to the README.md towards the bottom of the webpage for full instructions on installation and usage.</p>
+            </VStack>
         </VStack>
     )
 }
