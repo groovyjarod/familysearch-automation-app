2b87e15452c3452b9e0630e52e52900eece5e5b8
groovyjarod <jarodd99@gmail.com>
2026-06-18 14:31:22 -0600

    fixed overflow, height, colors of variables, and other code

--- reusables/BodyVstackCss.js
+++ reusables/BodyVstackCss.js
@@ -1,6 +1,5 @@
 const BodyVstackCss = {
     width: '100%',
-    height: '100%',
     maxWidth: '700px',
     gap: '4px',
     justifyContent: 'start',
@@ -8,7 +7,6 @@ const BodyVstackCss = {
     className: 'main',
     marginLeft: '2rem',
     marginRight: '2rem',
-    // overflowY: 'hidden'
 }
 
 export default BodyVstackCss
